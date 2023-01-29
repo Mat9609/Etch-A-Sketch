dd7ec4bdd279248d5ca56b8e9b961ba83bf404a8
Mat9609 <57141098+Mat9609@users.noreply.github.com>
2023-01-29 21:42:11 +0100

    Add Neon lights, Clear Button, UI composition

--- script2.js
+++ script2.js
@@ -1,56 +1,64 @@
-const grid = document.getElementById('grid');
+const grid = document.getElementById("grid");
 let size = 16;
 let color = "black";
-let drawMode = 'draw';
+let drawMode = "draw";
 let mouseDown = false;
-document.body.onmousedown = function () {mouseDown = true};
-document.body.onmouseup = function () {mouseDown = false};
-document.getElementById('colorPicker').oninput = (e) => setColor(e.target.value);
-const sizeValue = document.getElementById('sizeValue');
-const sizeSlider = document.getElementById('sizeSlider');
+document.body.onmousedown = function () {
+  mouseDown = true;
+};
+document.body.onmouseup = function () {
+  mouseDown = false;
+};
+document.getElementById("colorPicker").oninput = (e) =>
+  setColor(e.target.value);
+const sizeValue = document.getElementById("sizeValue");
+const sizeSlider = document.getElementById("sizeSlider");
+const clearButton = document.getElementById("clear");
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
 sizeSlider.onchange = (e) => changeSize(e.target.value);
+clearButton.onclick = (e) => clearGrid();
 
-
-function setColor(newColor){
-    color = newColor;
+function setColor(newColor) {
+  color = newColor;
 }
 
-function changeSize(newSize){
-    size = newSize;
-    grid.innerHTML = '';
-    setupGrid(size);
+function changeSize(newSize) {
+  size = newSize;
+  grid.innerHTML = "";
+  setupGrid(size);
 }
 
-function updateSizeValue(sizeValueText){
-    sizeValue.innerHTML = `${sizeValueText} x ${sizeValueText}`;
+function clearGrid() {
+  grid.innerHTML = "";
+  setupGrid(size);
+}
+
+function updateSizeValue(sizeValueText) {
+  sizeValue.innerHTML = `${sizeValueText} x ${sizeValueText}`;
 }
 
 function changeColor(e) {
-    if (e.type === 'mouseover' && !mouseDown) return;
-    else if (drawMode === 'draw') {
-      e.target.style.backgroundColor = color;
-    } else if (currentMode === 'eraser') {
-      e.target.style.backgroundColor = '#fefefe';
-    }
+  if (e.type === "mouseover" && !mouseDown) return;
+  else if (drawMode === "draw") {
+    e.target.style.backgroundColor = color;
+  } else if (currentMode === "eraser") {
+    e.target.style.backgroundColor = "#fefefe";
   }
+}
 
+function setupGrid(size) {
+  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size * size; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.addEventListener("mouseover", changeColor);
+    square.addEventListener("mousedown", changeColor);
+    grid.appendChild(square);
+  }
+}
 
-function setupGrid(size){
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size * size; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        square.addEventListener('mouseover', changeColor);
-        square.addEventListener('mousedown', changeColor);
-        grid.appendChild(square);
-    }
-};
-
-
-setupGrid(size);
-updateSizeValue(size);
-
-
+window.onload = () => {
+  setupGrid(16);
+}
 
